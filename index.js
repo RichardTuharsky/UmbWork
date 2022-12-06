ac260bf594a74dfda0343991f3fd19d7306cb34f
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-12-06 21:00:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var k = " ";
+    function Posts() {
+        var input = document.getElementsByName('input');
+
+        
+        const date = new Date();
+        let hour = date.getHours();
+        let minute = date.getMinutes();
+        let second = date.getSeconds();
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+        
+        for (var i = 0; i < input.length; i++) {
+                var a = input[i];
+                k = k + a.value + " [" + day + "/" + month + "/" + year + ", " + hour + ":" + minute + ":" + second + " AM" + "]:" + " ";
+        }
+        document.getElementById("par").innerHTML = k;
+        document.getElementById("po").innerHTML = " \n";
+}
