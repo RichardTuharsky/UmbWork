50a5755f8565a6c78e2cfaca47dc7b98ffa0cdf9
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-12-07 19:09:45 +0100

    .

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var k = " ";
-    function Posts() {
+var k = " \n ";
+function Posts() {
         var input = document.getElementsByName('input');
-
-        
         const date = new Date();
         let hour = date.getHours();
         let minute = date.getMinutes();
@@ -13,8 +11,11 @@ var k = " ";
         
         for (var i = 0; i < input.length; i++) {
                 var a = input[i];
-                k = k + a.value + " [" + day + "/" + month + "/" + year + ", " + hour + ":" + minute + ":" + second + " AM" + "]:" + " ";
+                k = k + a.value + " [" + day + "/" + month + "/" + year + ", " + hour + ":" + minute + ":" + second + " AM" + "]: " + " ";
         }
         document.getElementById("par").innerHTML = k;
-        document.getElementById("po").innerHTML = " \n";
-}
+        document.getElementById("po").innerHTML = " ";
+    }
+
+
+    
